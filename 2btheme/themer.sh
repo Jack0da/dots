1e199db5ca8146b33f1c4c7242ee6bf4c10fb3eb
Jack0da <jack@bsbdinc.com>
2018-03-14 13:24:28 -0400

    first commit

---
+++ 2btheme/themer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+########################
+# Quick theme selecting#
+# program by jack0da   #
+# for 2bwm             #
+########################
+PATHTO2bwm=$HOME/2bwm/ #Change to where your 2bwm dir is
+
+echo "Welcome to jack0da's 2bwm themer! Press [RETURN] to continue"
+read null ###just letting the user press enter before beginning. The program will function without it ###
+echo "Choose theme: [1]Icecrm [2]Solar [3]Coffee [4]Moon [*]Exit"
+read theme
+case $theme in
+	1)
+	  echo "'Icecrm' selected"
+	  cd icecrm;;
+	2)
+	  echo "'Solar' selected"
+	  cd solar;;
+	3)
+	  echo "'Coffee' selected"
+	  cd coffee;;
+	4)
+	  echo "'Moon' selected"
+	  cd moon;;
+	*)
+	  exit;;
+esac
+cp config $HOME/.config/polybar/
+cp config.h $PATHTO2bwm
+cp .xinitrc $HOME/
+cd $PATHTO2bwm
+make
+sudo make install
+echo "Done! logout to apply changes"
